b46c8de6f3bdfec389bfa4a086badd1b43ade90e
Kwaku Prempeh <kwaku.prempeh@holition.com>
2020-01-21 00:30:31 +0000

    update to script

--- tools/schematics/monorepo-setup/index.ts
+++ tools/schematics/monorepo-setup/index.ts
@@ -8,13 +8,33 @@ import {
   Tree,
   url
 } from '@angular-devkit/schematics';
+import { addDepsToPackageJson, updateJsonInTree } from '@nrwl/workspace';
 
 export default function(): Rule {
   return (host: Tree, context: SchematicContext) => {
     context.logger.debug('----- Monorepo Setup ------');
-    return chain([mergeWith(apply(url('./files'), [move(host.root.path)]))])(
-      host,
-      context
-    );
+    const dependencies = {};
+    const devDependencies = {
+      '@commitlint/cli': '^8.2.0',
+      '@commitlint/config-conventional': '^8.2.0',
+      '@commitlint/prompt': '*',
+      commitizen: '^4.0.3',
+      'cz-conventional-changelog': '^3.0.2',
+      husky: '^3.0.9',
+      lerna: '^3.18.3',
+      'lint-staged': '^9.4.2'
+    };
+    const shouldRunInstallTask = true;
+    return chain([
+      addDepsToPackageJson(dependencies, devDependencies, shouldRunInstallTask),
+      updateJsonInTree('package.json', json => {
+        json.scripts['----- SCRIPTS ---------'] = '';
+        json.scripts.release = 'lerna version';
+        json.scripts.commit = 'git-cz';
+        json.scripts['----- SCRIPTS END -----'] = '';
+        return json;
+      }),
+      mergeWith(apply(url('./files'), [move(host.root.path)]))
+    ])(host, context);
   };
 }
